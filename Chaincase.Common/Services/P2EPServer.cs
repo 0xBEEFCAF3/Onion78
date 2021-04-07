d57fa5272fa8de42ad0df478a080d11adffe9c5b
DanGould <d@ngould.dev>
2021-04-07 15:50:33 -0400

    v0.5.9

--- Chaincase.Common/Services/P2EPServer.cs
+++ Chaincase.Common/Services/P2EPServer.cs
@@ -61,7 +61,7 @@ namespace Chaincase.Common.Services
 
 		protected override async Task ExecuteAsync(CancellationToken stoppingToken)
 		{
-			var handler = new P2EPRequestHandler(Global.Network, Global.WalletManager, Global.Config.PrivacyLevelSome);
+			var handler = new P2EPRequestHandler(Global.Network, Global.WalletManager, 1);
 
 			while (!stoppingToken.IsCancellationRequested)
 			{
